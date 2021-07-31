e67b94ec67c83323f2a6d52b8d1141d78d610644
Liangchen <leeonky@gmail.com>
2021-07-31 17:56:39 +0800

    Merge class to one source file

--- src/main/java/com/github/leeonky/dal/token/TextParser.java
+++ src/main/java/com/github/leeonky/dal/token/TextParser.java
@@ -40,3 +40,43 @@ public abstract class TextParser {
 
     protected abstract String getContent(StringBuffer stringBuffer);
 }
+
+class SingleQuotationStringParser extends QuotationStringParser {
+
+    SingleQuotationStringParser() {
+        super('\'');
+    }
+
+    @Override
+    protected String escape(char c) {
+        switch (c) {
+            case '\'':
+            case '\\':
+                return String.valueOf(c);
+            default:
+                return "\\" + c;
+        }
+    }
+}
+
+class DoubleQuotationStringParser extends QuotationStringParser {
+
+    protected DoubleQuotationStringParser() {
+        super('"');
+    }
+
+    @Override
+    protected String escape(char c) {
+        switch (c) {
+            case '\\':
+            case '"':
+                return String.valueOf(c);
+            case 't':
+                return "\t";
+            case 'n':
+                return "\n";
+            default:
+                return "\\" + c;
+        }
+    }
+}
--- src/main/java/com/github/leeonky/dal/token/TokenFactory.java
+++ src/main/java/com/github/leeonky/dal/token/TokenFactory.java
@@ -3,3 +3,26 @@ package com.github.leeonky.dal.token;
 public interface TokenFactory {
     Token fetchToken(SourceCode sourceCode);
 }
+
+class SingleQuotationStringTokenFactory extends QuotationTokenFactory {
+
+    public SingleQuotationStringTokenFactory() {
+        super('\'', "string should end with `'`");
+    }
+
+    @Override
+    protected TextParser createParser() {
+        return new SingleQuotationStringParser();
+    }
+}
+
+class DoubleQuotationStringTokenFactory extends QuotationTokenFactory {
+    public DoubleQuotationStringTokenFactory() {
+        super('"', "string should end with `\"`");
+    }
+
+    @Override
+    protected TextParser createParser() {
+        return new DoubleQuotationStringParser();
+    }
+}
