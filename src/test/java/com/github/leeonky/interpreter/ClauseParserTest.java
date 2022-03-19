07d2e963b3e2a0cd30e59869fd3cf914d6c2192e
Liangchen <leeonky@gmail.com>
2022-03-19 18:52:08 +0800

    Add more test

--- src/test/java/com/github/leeonky/interpreter/ClauseParserTest.java
+++ src/test/java/com/github/leeonky/interpreter/ClauseParserTest.java
@@ -3,6 +3,7 @@ package com.github.leeonky.interpreter;
 import org.junit.jupiter.api.Test;
 
 import static com.github.leeonky.interpreter.ClauseParser.lazy;
+import static java.util.Optional.empty;
 import static java.util.Optional.of;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -52,5 +53,85 @@ class ClauseParserTest extends BaseTest {
         assertThat(clauseParser.concat(clauseParser2)
                 .parse(testProcedure)
                 .get().expression(node)).isSameAs(expression2);
+    }
+
+    @Test
+    void concat_with_another_empty_clause_parser() {
+        TestProcedure testProcedure = givenProcedureWithCode("");
+
+        Clause<TestContext, TestNode> clause = mock(Clause.class);
+
+        ClauseParser<TestContext, TestNode, TestExpression, TestOperator, TestProcedure> clauseParser = procedure -> {
+            assertThat(procedure).isSameAs(testProcedure);
+            return of(clause);
+        };
+
+        ClauseParser<TestContext, TestNode, TestExpression, TestOperator, TestProcedure> clauseParser2 = procedure -> {
+            assertThat(procedure).isSameAs(testProcedure);
+            return empty();
+        };
+
+        assertThat(clauseParser.concat(clauseParser2)
+                .parse(testProcedure)
+                .get()).isSameAs(clause);
+    }
+
+    @Test
+    void empty_concat_with_any_clause_parser() {
+        TestProcedure testProcedure = givenProcedureWithCode("");
+        ClauseParser<TestContext, TestNode, TestExpression, TestOperator, TestProcedure> clauseParser = procedure -> {
+            assertThat(procedure).isSameAs(testProcedure);
+            return empty();
+        };
+
+        assertThat(clauseParser.concat(mock(ClauseParser.class))
+                .parse(testProcedure)).isEmpty();
+    }
+
+    @Test
+    void default_input() {
+        TestProcedure testProcedure = givenProcedureWithCode("");
+
+        Clause<TestContext, TestNode> clause = mock(Clause.class);
+        TestNode node = new TestNode();
+
+        TestNode expression = new TestNode();
+        when(clause.expression(node)).thenReturn(expression);
+
+        ClauseParser<TestContext, TestNode, TestExpression, TestOperator, TestProcedure> clauseParser = procedure -> {
+            assertThat(procedure).isSameAs(testProcedure);
+            return of(clause);
+        };
+
+        assertThat(clauseParser.defaultInputNode(node).parse(testProcedure).get()).isSameAs(expression);
+    }
+
+    @Test
+    void empty_default_input() {
+        TestProcedure testProcedure = givenProcedureWithCode("");
+        ClauseParser<TestContext, TestNode, TestExpression, TestOperator, TestProcedure> clauseParser = procedure -> {
+            assertThat(procedure).isSameAs(testProcedure);
+            return empty();
+        };
+
+        assertThat(clauseParser.defaultInputNode(null).parse(testProcedure)).isEmpty();
+    }
+
+    @Test
+    void mandatory_with_input() {
+        TestProcedure testProcedure = givenProcedureWithCode("");
+
+        Clause<TestContext, TestNode> clause = mock(Clause.class);
+        TestNode node = new TestNode();
+
+        TestNode expression = new TestNode();
+        when(clause.expression(node)).thenReturn(expression);
+
+        ClauseParser.Mandatory<TestContext, TestNode, TestExpression, TestOperator, TestProcedure> mandatory = procedure -> {
+            assertThat(procedure).isSameAs(testProcedure);
+            return clause;
+        };
+
+        assertThat(mandatory.input(node).parse(testProcedure)).isSameAs(expression);
     }
 }
